03b2c302f84220edbc04aa800b66a6731bc452f6
vnshamsika <vnshamsika@gmail.com>
2026-04-30 19:59:52 +0530

    Update FIFO.java

--- OS/FIFO.java
+++ OS/FIFO.java
@@ -2,26 +2,41 @@ import java.util.*;
 
 public class FIFO {
     public static void main(String[] args) {
-        int[] pages={1,2,3,4,1,2,5,1,2,3,4,5};
-        int capacity=3;
-        Queue<Integer> queue= new LinkedList<>();
-        Set<Integer> set=new HashSet<>();
-        int faults=0;
-        for(int page: pages)
-        {
-            if(!set.contains(page))
-            {
-                faults++;
-                if(queue.size()==capacity)
-                {
-                    int removed=queue.poll();
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of pages: ");
+        int n = sc.nextInt();
+
+        int[] pages = new int[n];
+        System.out.println("Enter page reference string:");
+        for (int i = 0; i < n; i++) {
+            pages[i] = sc.nextInt();
+        }
+
+        System.out.print("Enter number of frames: ");
+        int capacity = sc.nextInt();
+
+        Queue<Integer> queue = new LinkedList<>();
+        Set<Integer> set = new HashSet<>();
+
+        int pageFaults = 0;
+
+        for (int page : pages) {
+            if (!set.contains(page)) {
+                pageFaults++;
+
+                if (queue.size() == capacity) {
+                    int removed = queue.poll();
                     set.remove(removed);
                 }
-                set.add(page);
+
                 queue.add(page);
+                set.add(page);
             }
-            System.out.println("Frames: "+queue);
+
+            System.out.println("Frames: " + queue);
         }
-        System.out.println("Total No. of Faults: "+ faults );
+
+        System.out.println("Total Page Faults (FIFO): " + pageFaults);
     }
 }
