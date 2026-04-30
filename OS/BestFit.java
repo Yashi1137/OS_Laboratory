1b1e7415d78f56fa583668830dd3e4ae512a4645
vnshamsika <vnshamsika@gmail.com>
2026-04-30 20:05:26 +0530

    Update BestFit.java

--- OS/BestFit.java
+++ OS/BestFit.java
@@ -1,52 +1,63 @@
 import java.util.*;
+
 public class BestFit {
-    public static void mian(String[] args)
-    {
-        Scanner sc= new Scanner(System.in);
-        int m=sc.nextInt();
-        int n=sc.nextInt();
-        int[] blockSize=new int[m];
-        int[] processSize= new int[n];
-        for(int i=0;i<n;i++)
-        {
-            processSize[i]=sc.nextInt();
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        
+        System.out.print("Enter number of memory blocks: ");
+        int m = sc.nextInt();
+
+        System.out.print("Enter number of processes: ");
+        int n = sc.nextInt();
+
+        int[] blockSize = new int[m];
+        int[] processSize = new int[n];
+
+        
+        System.out.println("Enter sizes of memory blocks:");
+        for (int i = 0; i < m; i++) {
+            blockSize[i] = sc.nextInt();
         }
-        for(int i=0;i<m;i++)
-        {
-            blockSize[i]=sc.nextInt();
-        } 
-        int[] allocation= new int[n];
-        Arrays.fill(allocation,-1);
-        for(int i=0;i<n;i++)
-        {
-            int bestIdx=-1;
-            for(int j=0;j<m;j++)
-            {
-                if(blockSize[j]>processSize[i])
-                {
-                    if(bestIdx==-1||blockSize[j]<blockSize[bestIdx])
-                    {
-                            bestIdx=j;
+
+       
+        System.out.println("Enter sizes of processes:");
+        for (int i = 0; i < n; i++) {
+            processSize[i] = sc.nextInt();
+        }
+
+        int[] allocation = new int[n];
+        Arrays.fill(allocation, -1);
+
+        
+        for (int i = 0; i < n; i++) {
+            int bestIdx = -1;
+
+            for (int j = 0; j < m; j++) {
+                if (blockSize[j] >= processSize[i]) {
+                    if (bestIdx == -1 || blockSize[j] < blockSize[bestIdx]) {
+                        bestIdx = j;
                     }
                 }
             }
-            if(bestIdx!=-1)
-            {
-                allocation[i]=bestIdx;
-                blockSize[bestIdx]-=processSize[i];
+
+            if (bestIdx != -1) {
+                allocation[i] = bestIdx;
+                blockSize[bestIdx] -= processSize[i];
             }
         }
-        System.out.println("Process No.\tProcess Size\tBlock No.");
-        for(int i=0;i<n;i++)
-        {
-            System.out.println((i+1)+"\t\t"+processSize[i]+"\t\t");
-            if(allocation[i]!=-1)
-            {
-                System.out.print(allocation[i]+1);
-            }else{
-                System.out.print("Not allocated");
+
+        
+        System.out.println("\nProcess No.\tProcess Size\tBlock No.");
+        for (int i = 0; i < n; i++) {
+            System.out.print((i + 1) + "\t\t" + processSize[i] + "\t\t");
+            if (allocation[i] != -1) {
+                System.out.println(allocation[i] + 1);
+            } else {
+                System.out.println("Not Allocated");
             }
         }
+
         sc.close();
     }
 }
