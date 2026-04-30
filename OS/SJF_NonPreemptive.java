1aad075d2f0a436a6426a1f9fc337d0ef42bbfcc
vnshamsika <vnshamsika@gmail.com>
2026-04-30 21:50:39 +0530

    Update SJF_NonPreemptive.java

--- OS/SJF_NonPreemptive.java
+++ OS/SJF_NonPreemptive.java
@@ -1,45 +1,56 @@
 import java.util.*;
 
-public class SJF_NoArrival {
+class SJF_NonPreemptive {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
         System.out.print("Enter number of processes: ");
         int n = sc.nextInt();
 
+        int[] at = new int[n];
         int[] bt = new int[n];
         int[] wt = new int[n];
         int[] tat = new int[n];
+        boolean[] done = new boolean[n];
 
-        System.out.println("Enter burst times:");
-        for (int i = 0; i < n; i++) {
+        System.out.println("Enter Arrival Time:");
+        for (int i = 0; i < n; i++)
+            at[i] = sc.nextInt();
+
+        System.out.println("Enter Burst Time:");
+        for (int i = 0; i < n; i++)
             bt[i] = sc.nextInt();
-        }
 
-        // Sorting burst times
-        for (int i = 0; i < n; i++) {
-            for (int j = i + 1; j < n; j++) {
-                if (bt[i] > bt[j]) {
-                    int temp = bt[i];
-                    bt[i] = bt[j];
-                    bt[j] = temp;
+        int time = 0, completed = 0;
+
+        while (completed < n) {
+            int idx = -1;
+            int minBT = Integer.MAX_VALUE;
+
+            for (int i = 0; i < n; i++) {
+                if (at[i] <= time && !done[i] && bt[i] < minBT) {
+                    minBT = bt[i];
+                    idx = i;
                 }
+            }
+
+            if (idx == -1) {
+                time++;
+            } else {
+                wt[idx] = time - at[idx];
+                time += bt[idx];
+                tat[idx] = wt[idx] + bt[idx];
+                done[idx] = true;
+                completed++;
             }
         }
 
-        wt[0] = 0;
-
-        for (int i = 1; i < n; i++) {
-            wt[i] = wt[i - 1] + bt[i - 1];
-        }
-
+        System.out.println("\nP\tAT\tBT\tWT\tTAT");
         for (int i = 0; i < n; i++) {
-            tat[i] = wt[i] + bt[i];
+            System.out.println((i+1) + "\t" + at[i] + "\t" + bt[i] + "\t" +
+                               wt[i] + "\t" + tat[i]);
         }
 
-        System.out.println("\nProcess\tBT\tWT\tTAT");
-        for (int i = 0; i < n; i++) {
-            System.out.println((i+1) + "\t" + bt[i] + "\t" + wt[i] + "\t" + tat[i]);
-        }
+        sc.close();
     }
 }
