cd3d7b3352d9a891a2d88552707b3605fa3e0ebc
vnshamsika <vnshamsika@gmail.com>
2026-04-30 21:49:21 +0530

    Update SJF_Preemptive.java

--- OS/SJF_Preemptive.java
+++ OS/SJF_Preemptive.java
@@ -1,25 +1,26 @@
 import java.util.*;
 
-class ProcessP {
-    int id, at, bt, rt, ct, tat, wt;
-}
-
-public class SJF_Preemptive {
+class SJF_P_NoCT {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
         System.out.print("Enter number of processes: ");
         int n = sc.nextInt();
 
-        ProcessP[] p = new ProcessP[n];
+        int[] at = new int[n];
+        int[] bt = new int[n];
+        int[] rt = new int[n];
+        int[] wt = new int[n];
+        int[] tat = new int[n];
 
+        System.out.println("Enter Arrival Time:");
+        for (int i = 0; i < n; i++)
+            at[i] = sc.nextInt();
+
+        System.out.println("Enter Burst Time:");
         for (int i = 0; i < n; i++) {
-            p[i] = new ProcessP();
-            p[i].id = i;
-            System.out.print("Enter AT and BT for P" + i + ": ");
-            p[i].at = sc.nextInt();
-            p[i].bt = sc.nextInt();
-            p[i].rt = p[i].bt;
+            bt[i] = sc.nextInt();
+            rt[i] = bt[i];
         }
 
         int time = 0, completed = 0;
@@ -29,8 +30,8 @@ public class SJF_Preemptive {
             int minRT = Integer.MAX_VALUE;
 
             for (int i = 0; i < n; i++) {
-                if (p[i].at <= time && p[i].rt > 0 && p[i].rt < minRT) {
-                    minRT = p[i].rt;
+                if (at[i] <= time && rt[i] > 0 && rt[i] < minRT) {
+                    minRT = rt[i];
                     idx = i;
                 }
             }
@@ -38,22 +39,23 @@ public class SJF_Preemptive {
             if (idx == -1) {
                 time++;
             } else {
-                p[idx].rt--;
+                rt[idx]--;
                 time++;
 
-                if (p[idx].rt == 0) {
-                    p[idx].ct = time;
-                    p[idx].tat = p[idx].ct - p[idx].at;
-                    p[idx].wt = p[idx].tat - p[idx].bt;
+                if (rt[idx] == 0) {
                     completed++;
+
+                    int finish = time; // internal only
+                    tat[idx] = finish - at[idx];
+                    wt[idx] = tat[idx] - bt[idx];
                 }
             }
         }
 
-        System.out.println("\nP\tAT\tBT\tCT\tTAT\tWT");
-        for (ProcessP x : p) {
-            System.out.println("P" + x.id + "\t" + x.at + "\t" + x.bt +
-                    "\t" + x.ct + "\t" + x.tat + "\t" + x.wt);
+        System.out.println("\nP\tAT\tBT\tWT\tTAT");
+        for (int i = 0; i < n; i++) {
+            System.out.println((i+1) + "\t" + at[i] + "\t" + bt[i] + "\t" +
+                               wt[i] + "\t" + tat[i]);
         }
 
         sc.close();
