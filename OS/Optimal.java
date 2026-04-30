3a18f1b8c9b3dc8766d9455c139ae9dad5f87725
vnshamsika <vnshamsika@gmail.com>
2026-04-30 20:01:08 +0530

    Update Optimal.java

--- OS/Optimal.java
+++ OS/Optimal.java
@@ -1,48 +1,59 @@
 import java.util.*;
+
 public class Optimal {
     public static void main(String[] args) {
-        int[] pages= { 1,2,3,4,1,2,5,1,2,3,4,5};
-        int capacity=3;
-        List<Integer> frames= new ArrayList<>();
-        int faults=0;
-        for(int i=0;i<pages.length;i++)
-        {
-            int page=pages[i];
-            if(!frames.contains(page))
-            {
-                faults++;
-                if(capacity>frames.size())
-                {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of pages: ");
+        int n = sc.nextInt();
+
+        int[] pages = new int[n];
+        System.out.println("Enter page reference string:");
+        for (int i = 0; i < n; i++) {
+            pages[i] = sc.nextInt();
+        }
+
+        System.out.print("Enter number of frames: ");
+        int capacity = sc.nextInt();
+
+        List<Integer> frames = new ArrayList<>();
+        int pageFaults = 0;
+
+        for (int i = 0; i < n; i++) {
+            int page = pages[i];
+
+            if (!frames.contains(page)) {
+                pageFaults++;
+
+                if (frames.size() < capacity) {
                     frames.add(page);
-                }else{
-                    int indexToRemove=-1;
-                    int farthest=i;
-                    for(int j=0;j<frames.size();j++)
-                    {
+                } else {
+                    int farthest = i;
+                    int index = -1;
+
+                    for (int j = 0; j < frames.size(); j++) {
                         int k;
-                        for(k=i+1;k<pages.length;k++)
-                        {
-                            if(frames.get(j)==pages[k])
-                            {
-                                if(farthest<k)
-                                {
-                                    farthest=k;
-                                    indexToRemove=j;
-                                }
+                        for (k = i + 1; k < n; k++) {
+                            if (frames.get(j) == pages[k])
                                 break;
-                            }
                         }
-                        if(k==pages.length)
-                        {
-                            indexToRemove=j;
-                            break;
+
+                        if (k > farthest) {
+                            farthest = k;
+                            index = j;
                         }
                     }
-                    frames.set(indexToRemove,page);
+
+                    if (index == -1)
+                        frames.set(0, page);
+                    else
+                        frames.set(index, page);
                 }
             }
-            System.out.println("Frames: "+frames);
+
+            System.out.println("Frames: " + frames);
         }
-        System.out.println("Total faults: "+faults);
+
+        System.out.println("Total Page Faults (Optimal): " + pageFaults);
     }
 }
