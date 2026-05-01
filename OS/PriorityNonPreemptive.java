323ca125f1f3efa24f6d413ff27eee57d8e7e9d3
vnshamsika <vnshamsika@gmail.com>
2026-05-01 07:23:48 +0530

    Update PriorityNonPreemptive.java

--- OS/PriorityNonPreemptive.java
+++ OS/PriorityNonPreemptive.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-class Process {
+class Proc {
     int id, at, bt, pr, ct, tat, wt;
 }
 
@@ -11,12 +11,14 @@ public class PriorityNonPreemptive {
         System.out.print("Enter number of processes: ");
         int n = sc.nextInt();
 
-        Process[] p = new Process[n];
+        Proc[] p = new Proc[n];
 
+        // Input
         for (int i = 0; i < n; i++) {
-            p[i] = new Process();
+            p[i] = new Proc();
             p[i].id = i;
-            System.out.print("AT BT Priority for P" + i + ": ");
+
+            System.out.print("Enter AT BT Priority for P" + i + ": ");
             p[i].at = sc.nextInt();
             p[i].bt = sc.nextInt();
             p[i].pr = sc.nextInt();
@@ -24,7 +26,6 @@ public class PriorityNonPreemptive {
 
         int time = 0, completed = 0;
         boolean[] done = new boolean[n];
-
         List<Integer> order = new ArrayList<>();
 
         while (completed < n) {
@@ -34,9 +35,10 @@ public class PriorityNonPreemptive {
             for (int i = 0; i < n; i++) {
                 if (!done[i] && p[i].at <= time) {
 
-                    // 🔥 Tie-breaking added
-                    if (p[i].pr < minPr || 
-                       (p[i].pr == minPr && p[i].at < p[idx].at)) {
+                    if (idx == -1 || 
+                        p[i].pr < minPr || 
+                        (p[i].pr == minPr && p[i].at < p[idx].at)) {
+                        
                         minPr = p[i].pr;
                         idx = i;
                     }
@@ -58,19 +60,29 @@ public class PriorityNonPreemptive {
             }
         }
 
-        // 🔹 Execution Order (Gantt style)
+        // 🔹 Execution Order
         System.out.print("\nExecution Order: ");
         for (int i = 0; i < order.size(); i++) {
             System.out.print("P" + order.get(i));
             if (i != order.size() - 1) System.out.print(" -> ");
         }
 
-        // 🔹 Table
+        // 🔹 Table Output
         System.out.println("\n\nP\tAT\tBT\tPR\tCT\tTAT\tWT");
-        for (Process x : p) {
+
+        double totalTAT = 0, totalWT = 0;
+
+        for (Proc x : p) {
             System.out.println("P" + x.id + "\t" + x.at + "\t" + x.bt + "\t" + x.pr +
                     "\t" + x.ct + "\t" + x.tat + "\t" + x.wt);
+
+            totalTAT += x.tat;
+            totalWT += x.wt;
         }
+
+        // 🔹 Averages
+        System.out.println("\nAverage TAT = " + (totalTAT / n));
+        System.out.println("Average WT  = " + (totalWT / n));
 
         sc.close();
     }
